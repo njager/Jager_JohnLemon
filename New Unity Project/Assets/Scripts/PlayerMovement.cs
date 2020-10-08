4f7ffb51393b3750bf96f469acebc43faae35caa
njager <nicholasejager@gmail.com>
2020-10-08 17:12:24 -0400

    added rotation variable and vectors and set them

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     //member variables
     Animator m_Animator;
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    //public variables
+    public float turnSpeed = 20f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +24,18 @@ public class PlayerMovement : MonoBehaviour
         //gathers input data from WASD
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+       
         //sets the movement vector to the input data and normalizes magnitude to 1
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize ();
-        //detects movement and sets a bool accordingly
+       
+        //detects movement and sets a bool accordingly that then sets the internal animator bool
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
     }
 }
