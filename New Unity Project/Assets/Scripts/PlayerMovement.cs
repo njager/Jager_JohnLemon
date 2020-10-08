0aabc298b4029033b62de462f37b829892caa4b3
njager <nicholasejager@gmail.com>
2020-10-08 16:52:36 -0400

    input movement detection

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //member variables
     Vector3 m_Movement;
 
     // Start is called before the first frame update
@@ -15,9 +16,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //gathers input data from WASD
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        //sets the movement vector to the input data and normalizes magnitude to 1
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize ();
+        //detects movement and sets a bool accordingly
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
     }
 }
