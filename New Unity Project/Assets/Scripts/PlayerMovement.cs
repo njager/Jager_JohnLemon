f93db3f2766d9409da0a33c296cdf52496dbde8f
njager <nicholasejager@gmail.com>
2020-10-08 16:57:25 -0400

    called animator component in movement script

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //member variables
+    Animator m_Animator;
     Vector3 m_Movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
