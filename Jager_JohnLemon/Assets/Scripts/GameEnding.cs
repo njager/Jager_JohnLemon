d738f6e11bcab5339b2902831da169fce0be1252
njager <nicholasejager@gmail.com>
2020-10-10 18:41:57 -0400

    added end level function and related variables

--- Jager_JohnLemon/Assets/Scripts/GameEnding.cs
+++ Jager_JohnLemon/Assets/Scripts/GameEnding.cs
@@ -7,9 +7,11 @@ public class GameEnding : MonoBehaviour
     //public variables
     public float fadeDuration = 1f;
     public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
 
     //member variables
     bool m_IsPlayerAtExit;
+    float m_Timer;
 
     //detect player hit win box
     private void OnTriggerEnter(Collider other)
@@ -20,11 +22,19 @@ public class GameEnding : MonoBehaviour
         }
     }
 
+    //is called every frame; checks for status changes
     private void Update()
     {
         if(m_IsPlayerAtExit)
         {
-
+            EndLevel();
         }
+    }
+
+    //activates the end screen and quits game
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
     }
 }
