0c391fec6414d8f0d447158121a711fd34ad7900
njager <nicholasejager@gmail.com>
2020-10-10 18:36:55 -0400

    added player at exit bool and set up to be recognized

--- Jager_JohnLemon/Assets/Scripts/GameEnding.cs
+++ Jager_JohnLemon/Assets/Scripts/GameEnding.cs
@@ -8,10 +8,21 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f;
     public GameObject player;
 
+    //member variables
+    bool m_IsPlayerAtExit;
+
     //detect player hit win box
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(m_IsPlayerAtExit)
         {
 
         }
