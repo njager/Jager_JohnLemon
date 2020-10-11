03f23f7ec83a6d72a2d012020014fc7bc19bad65
njager <nicholasejager@gmail.com>
2020-10-11 11:15:57 -0400

    added variables to Waypoint script; navmesh agent array

--- Jager_JohnLemon/Assets/Scripts/WaypointPatrol.cs
+++ Jager_JohnLemon/Assets/Scripts/WaypointPatrol.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
+    //public variables
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     // Update is called once per frame
