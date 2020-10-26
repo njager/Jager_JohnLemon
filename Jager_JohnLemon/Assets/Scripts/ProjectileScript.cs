201c51e37b72b81a43f02e11d4db113bff3019a6
njager <nicholasejager@gmail.com>
2020-10-25 21:42:29 -0400

    added audio source with splash to projectile

--- Jager_JohnLemon/Assets/Scripts/ProjectileScript.cs
+++ Jager_JohnLemon/Assets/Scripts/ProjectileScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour
 {
     public GameObject projectile;
-
+    AudioSource audioSource;
+    public AudioClip splashSFX;
 
     private void Start()
     {
         Destroy(gameObject, 5f);
+        audioSource = GetComponent<audioSource>();
         
     }
     //detects bullet collision and deals damage to enemies
